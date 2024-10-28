d605b74961b07ee95fc964a717378a6bed199ce9
akobirweb <akobiraripdjanov09@gmail.com>
2024-10-28 14:07:23 +0500

    Musiqa tugmalari qo'shildi va /start buyruqidagi xato tuzatilgan

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 // Last.fm API kalitini kiriting
-const lastFmApiKey = 'd3b805d9dbaf59a5649698c5e6d24d5d'; // Sizning Last.fm API kalitingiz
+const lastFmApiKey = 'd3b805d9dbaf59a5649698c5e6d24d5d'; // O'z Last.fm API kalitingizni joylashtiring
 
 // Bot tokenini kiriting
 const token = '7529923721:AAE7CoWB9aj1xCuW6cXwuotRHZRp_1YZlwM';
@@ -12,6 +12,24 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Salom! Musiqa nomini kiriting, men sizga musiqa haqida ma\'lumot beraman.');
 });
+
+// Musiqa ro'yxatini yaratish
+function createMusicButtons(tracks, chatId) {
+    const buttons = tracks.map((track, index) => ({
+        text: `${index + 1}: ${track.name} by ${track.artist}`,
+        callback_data: track.url // Musiqa URL'sini callback data sifatida olamiz
+    }));
+
+    const replyMarkup = {
+        inline_keyboard: [
+            buttons.map(button => [{ text: button.text, callback_data: button.callback_data }])
+        ]
+    };
+
+    bot.sendMessage(chatId, 'Musiqa ro\'yxati tayyor! Iltimos, ijro etish uchun raqamni bosing.', {
+        reply_markup: replyMarkup
+    });
+}
 
 // Foydalanuvchi musiqa nomini yozganda
 bot.on('message', (msg) => {
@@ -24,22 +42,8 @@ bot.on('message', (msg) => {
             const tracks = response.data.results.trackmatches.track;
 
             if (tracks.length > 0) {
-                // Musiqa topilganda, ularni raqamli tugmalar bilan ko'rsatamiz
-                let buttons = [];
-                tracks.forEach((track, index) => {
-                    if (index < 10) { // Faqat 10 ta musiqani ko'rsatamiz
-                        buttons.push([{ text: `${index + 1}: ${track.name} by ${track.artist}`, callback_data: `play_${index}` }]);
-                    }
-                });
-
-                // Musiqa ro'yxatini yuborish
-                const options = {
-                    reply_markup: {
-                        inline_keyboard: buttons
-                    }
-                };
-
-                bot.sendMessage(chatId, 'Musiqa topildi:', options);
+                // Musiqa topilganda
+                createMusicButtons(tracks, chatId); // Musiqa tugmalarini yaratamiz
             } else {
                 // Musiqa topilmasa
                 bot.sendMessage(chatId, 'Kechirasiz, bunday musiqa topilmadi.');
@@ -51,27 +55,13 @@ bot.on('message', (msg) => {
         });
 });
 
-// Foydalanuvchi tugmani bosganda
-bot.on('callback_query', (callbackQuery) => {
-    const chatId = callbackQuery.message.chat.id;
-    const action = callbackQuery.data;
+// Inline tugmalar bosilganda musiqa ijro etish
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+    const musicUrl = query.data; // Callback data sifatida olingan URL
 
-    // Agar foydalanuvchi musiqani ijro etishni xohlasa
-    if (action.startsWith('play_')) {
-        const trackIndex = action.split('_')[1];
+    const audio = new Audio(musicUrl); // Musiqani ijro etish
+    audio.play();
 
-        // Musiqa haqida qo'shimcha ma'lumot olish
-        axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${lastFmApiKey}&mbid=${tracks[trackIndex].mbid}&format=json`)
-            .then(response => {
-                const track = response.data.track;
-                const trackUrl = track.url; // Musiqa havolasi
-
-                // Musiqani yuborish
-                bot.sendMessage(chatId, `O'ynatish: ${track.name} by ${track.artist}\n${trackUrl}`);
-            })
-            .catch(error => {
-                console.error('API xatosi:', error);
-                bot.sendMessage(chatId, 'Musiqani ijro etishda xato yuz berdi.');
-            });
-    }
+    bot.sendMessage(chatId, 'Musiqa ijro etilmoqda!');
 });
